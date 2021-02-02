9925b9c2ab9910be1f0f8892048c29f0395909bb
LAPTOP-6UF1OQKL\anton <antonio.barahona@grupoprogestion.com>
2021-02-02 10:30:41 -0300

    ABM funcional

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item"><a class="btn btn-sm btn-primary" href="{{ route("post.create") }}">Crear</a></li>
+            <li class="nav-item"><a class="btn btn-sm btn-primary" href="{{ route("post.create") }}">+ NUEVO</a></li>
         </ul>
         <form action="{{ route('post.search') }}" method="POST" class="d-flex">
             {{ csrf_field() }}
@@ -25,6 +25,7 @@
         <thead>
             <tr>
                 <th>{{ "ID" }}</th>
+                <th>{{ "CATEGORIA" }}</th>
                 <th>{{ "IMAGE" }}</th>
                 <th>{{ "TITLE" }}</th>
                 <th>{{ "AUTHOR" }}</th>
@@ -36,6 +37,7 @@
                     @foreach ($posts as $post)
                         <tr>
                             <td scope="row">{{ $post->id }}</td>
+                            <td scope="row">{{ $post->category->name }}</td>
                             <td scope="row"><img src="{{ asset('storage').'/'.$post->image }}" alt="" width="100"></td>
                             <td>{{ $post->title }}</td>
                             <td>{{ $post->author }}</td>
